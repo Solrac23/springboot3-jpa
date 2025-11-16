b1c3c0f57d1b9b4748a5765544459e6a0b630df6
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-11-15 21:47:57 -0300

    feat: Exception handling - delete

--- src/main/java/com/programmer/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/programmer/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.programmer.course.services.exceptions.DatabaseException;
 import com.programmer.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,19 @@ public class ResourceExceptionHandler {
   public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
     String error = "Resource not found";
     HttpStatus status = HttpStatus.NOT_FOUND;
+    StandardError err = new StandardError();
+    err.setTimestamp(Instant.now());
+    err.setStatus(status.value());
+    err.setError(error);
+    err.setMessage(e.getMessage());
+    err.setPath(request.getRequestURI());
+    return ResponseEntity.status(status).body(err);
+  }
+
+   @ExceptionHandler(DatabaseException.class)
+  public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+    String error = "Database error";
+    HttpStatus status = HttpStatus.BAD_REQUEST;
     StandardError err = new StandardError();
     err.setTimestamp(Instant.now());
     err.setStatus(status.value());
